01511ba82711d59a794dc2d7b054b544c418f5d4
Will Blevins <willblev@gmail.com>
2022-02-27 18:42:47 +0100

    have a working NTP-based status expiration function

--- src/main.cpp
+++ src/main.cpp
@@ -66,6 +66,65 @@ void saveConfigCallback () {
   shouldSaveConfig = true;
 }
 
+void getFormattedTime(){
+  //print a formatted version of the current time
+  timeClient.update();
+  String formattedTime = timeClient.getFormattedTime();
+  DEBUG_SERIAL.println("Formatted Time: ");
+  DEBUG_SERIAL.println(formattedTime);  
+}
+
+int epochLastMidnight(){
+  // calculate the Epoch time at midnight of the current day, so we can calculate what future times will be
+  // eg 8PM today will be today's Epoch midnight plus 8*3600 seconds
+  timeClient.update();
+  int epochTime = timeClient.getEpochTime();
+  int hoursSinceMidnight = timeClient.getHours();
+  int minutesAfterHour = timeClient.getMinutes();
+  int secondsAfterMinute = timeClient.getSeconds();
+  int totalSecondsSinceMidnight = (hoursSinceMidnight*3600) + (minutesAfterHour*60) + secondsAfterMinute;
+  int epochAtLastMidnight = epochTime - totalSecondsSinceMidnight;
+  DEBUG_SERIAL.print("Hours since last midnight: ");
+  DEBUG_SERIAL.println(String(hoursSinceMidnight));  
+  DEBUG_SERIAL.print("Minutes since last hour: ");
+  DEBUG_SERIAL.println(String(minutesAfterHour));
+  DEBUG_SERIAL.print("seconds since last minute: ");
+  DEBUG_SERIAL.println(String(secondsAfterMinute));
+
+  DEBUG_SERIAL.print("Total Seconds since last midnight: ");
+  DEBUG_SERIAL.println(totalSecondsSinceMidnight);
+  DEBUG_SERIAL.print("Epoch Time at last midnight: ");
+  DEBUG_SERIAL.println(epochAtLastMidnight);
+
+  return(epochAtLastMidnight);  
+}
+
+int getEpochTime() {
+  timeClient.update();
+  int epochTime = timeClient.getEpochTime();
+  DEBUG_SERIAL.print("Epoch Time from NTP: ");
+  DEBUG_SERIAL.println(epochTime);
+  //epochLastMidnight(epochTime);
+  getFormattedTime();
+  return(epochTime);
+}
+
+int slackStatusUntilTime(int hours, int minutes){
+  // this function takes as input a 24h time split into hours and minutes
+  // e.g. 19:45 would be passed as secondsFromMidnightUntilTime(19,45)
+  // and it returns the epoch GMT 0 time that corresponds to the desired time
+  timeClient.update();
+  int epochAtDesiredTime = epochLastMidnight() + (hours*3600) + (minutes*60) - GMTOffsetSeconds;
+  // we need to remove the offset that we set before, otherwise we'll be off by N hours since Slack uses GMT 0 
+  DEBUG_SERIAL.print("The epoch GMT0 equivalent time for ");
+  DEBUG_SERIAL.print(hours);
+  DEBUG_SERIAL.print(":");
+  DEBUG_SERIAL.print(minutes);
+  DEBUG_SERIAL.print(" is: ");
+  DEBUG_SERIAL.println(epochAtDesiredTime);
+    return(epochAtDesiredTime);
+}
+
 void updateLEDs() {
   
   switch(btnPressCount){
@@ -121,7 +180,7 @@ void updateSlackAPI() { // if a change has been registered locally, but hasn't b
    switch(LEDstatus){  // depending on what color the LEDs currently are, change the Slack status to... 
     case 0: // off
     {
-      profile = slack.setCustomStatus(message0, emoji0);
+      profile = slack.setCustomStatus(message0, emoji0, timeout0);
       delay(50);
       slack.setPresence(SLACK_PRESENCE_AWAY);
       break;
@@ -129,7 +188,7 @@ void updateSlackAPI() { // if a change has been registered locally, but hasn't b
     
     case 1: // green
     {
-      profile = slack.setCustomStatus(message1, emoji1);
+      profile = slack.setCustomStatus(message1, emoji1, slackStatusUntilTime(18,42));
       delay(50);
       slack.setPresence(SLACK_PRESENCE_AUTO);
       break;
@@ -137,7 +196,7 @@ void updateSlackAPI() { // if a change has been registered locally, but hasn't b
 
     case 2: // yellow
     { 
-      profile = slack.setCustomStatus(message2, emoji2);
+      profile = slack.setCustomStatus(message2, emoji2, timeout2 + getEpochTime());
       delay(50);
       slack.setPresence(SLACK_PRESENCE_AUTO);
       break;
@@ -145,7 +204,7 @@ void updateSlackAPI() { // if a change has been registered locally, but hasn't b
     
     case 3: //red
     {
-      profile = slack.setCustomStatus(message3, emoji3);
+      profile = slack.setCustomStatus(message3, emoji3, timeout3 + getEpochTime());
       delay(50);
       slack.setPresence(SLACK_PRESENCE_AUTO);
       break;
@@ -226,42 +285,6 @@ void displayProfile(SlackProfile profile)
 }
 
 
-void getFormattedTime(){
-  //print a formatted version of the current time
-  String formattedTime = timeClient.getFormattedTime();
-  DEBUG_SERIAL.println("Formatted Time: ");
-  DEBUG_SERIAL.println(formattedTime);  
-}
-
-unsigned long whenWasLastMidnight(unsigned long epochTime){
-  // calculate the Epoch time at midnight of the current day, so we can calculate what future times will be
-  // eg 8PM today will be today's Epoch midnight plus 8*3600 seconds
-  struct tm *ptm = gmtime ((time_t *)&epochTime);
-  unsigned long hoursSinceMidnight = ptm->tm_hour;
-  unsigned long minutesAfterHour = ptm->tm_min;
-  unsigned long secondsAfterMinute = ptm->tm_sec;
-  unsigned long epochAtLastMidnight = epochTime - ((hoursSinceMidnight*3600) + (minutesAfterHour*60) + secondsAfterMinute);
-  DEBUG_SERIAL.print("Hours since last midnight: ");
-  DEBUG_SERIAL.println(String(hoursSinceMidnight));  
-  DEBUG_SERIAL.print("Minutes since last hour: ");
-  DEBUG_SERIAL.println(String(minutesAfterHour));
-  DEBUG_SERIAL.print("seconds since last minute: ");
-  DEBUG_SERIAL.println(String(secondsAfterMinute));
-
-  DEBUG_SERIAL.print("Epoch Time at last midnight: ");
-  DEBUG_SERIAL.println(epochAtLastMidnight);
-  getFormattedTime();
-  return(epochAtLastMidnight);  
-}
-
-unsigned long getEpochTime() {
-  timeClient.update();
-  unsigned long epochTime = timeClient.getEpochTime();
-  DEBUG_SERIAL.print("Epoch Time from NTP: ");
-  DEBUG_SERIAL.println(epochTime);
-  whenWasLastMidnight(epochTime);
-  return(epochTime);
-}
 
 
 
